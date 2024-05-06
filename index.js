a9a87b78c6c24b9116e943ade7a87388f9acf45f
unknown <whritenourc@outlook.com>
2024-05-06 15:33:56 -0500

    finished the basic game functionality

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ class Sprite {
         }
         this.color = color
         this.isAttacking
+        this.health = 100
     }
 
     draw() {
@@ -33,10 +34,10 @@ class Sprite {
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
 
         // attack box
-        // if (this.isAttacking) {
-        c.fillStyle = 'green'
-        c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
-        // }
+        if (this.isAttacking) {
+            c.fillStyle = 'green'
+            c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
+        }
 
     }
 
@@ -122,6 +123,35 @@ function rectangularCollison({ rectangle1, rectangle2 }) {
     )
 }
 
+function determineWinner({ player, enemy, timerId}) {
+    clearTimeout(timerId)
+    document.querySelector('#displayText').style.display = 'flex'
+    if (player.health === enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Tie'
+    } else if (player.health > enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 1 Wins'
+    } else if (player.health < enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
+    }
+}
+
+let timer = 60
+let timerId
+
+function decreaseTimer() {
+
+    if (timer > 0) {
+       timerId = setTimeout(decreaseTimer, 1000)
+        timer--
+        document.querySelector('#timer').innerHTML = timer
+    }
+    if (timer === 0) {
+        determineWinner({ player, enemy })
+    } 
+}
+
+decreaseTimer()
+
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
@@ -154,7 +184,8 @@ function animate() {
         player.isAttacking
     ) {
         player.isAttacking = false
-        console.log('player attack successful')
+        enemy.health -= 20
+        document.querySelector('#enemyHealth').style.width = enemy.health + '%'
     }
 
     if (rectangularCollison({
@@ -164,7 +195,13 @@ function animate() {
         enemy.isAttacking
     ) {
         enemy.isAttacking = false
-        console.log('enemy attack successful')
+        player.health -= 20
+        document.querySelector('#playerHealth').style.width = player.health + '%'
+    }
+
+    // end game based on health
+    if (enemy.health <= 0 || player.health <= 0) {
+        determineWinner({ player, enemy, timerId })
     }
 }
 
@@ -224,5 +261,5 @@ window.addEventListener('keyup', (event) => {
             keys.ArrowLeft.presssed = false
             break
     }
-   
+
 })
